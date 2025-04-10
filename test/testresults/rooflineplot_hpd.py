d323bd1a0a44732fe91b3060baa26d12df00226d
Nico <nico.voss@physik.uni-regensburg.de>
2025-04-10 16:29:41 +0200

    plots for bigger grid

--- test/testresults/rooflineplot_hpd.py
+++ test/testresults/rooflineplot_hpd.py
@@ -1,9 +1,9 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-base_lattice_dimensions = [16, 8, 8, 16]
-base_lattice_str = f"{base_lattice_dimensions[0]}x{base_lattice_dimensions[1]}x{base_lattice_dimensions[2]}x{base_lattice_dimensions[3]}"
-basevol = 16*8*8*16
+base_lattice_dim = [16, 16, 16, 16]
+base_lattice_str = f"{base_lattice_dim[0]}x{base_lattice_dim[1]}x{base_lattice_dim[2]}x{base_lattice_dim[3]}"
+basevol = base_lattice_dim[0]*base_lattice_dim[1]*base_lattice_dim[2]*base_lattice_dim[3]
 
 wilson_FLOP = basevol*5496
 # bandwidth of hpd: 19.6 GB/s per core * 4 cores (according to Dr. Bandwidth)
@@ -13,54 +13,45 @@ hpd_4core_FLOP_p_s = 2 * 4 * 4 * 2.4 * 10**9
 # as the above value does not agree with experiment, maybe we use execution units from 8 cores?
 hpd_8core_FLOP_p_s = 2 * 4 * 8 * 2.4 * 10**9
 
+# 16x8x8x16
+# data_8thr_sockets = [
+#     [       10027008,        1012.230,           1.774, [16, 8, 8, 8]],
+#     [       13172736,        1015.779,           2.485, [16, 8, 8, 12]],
+#     [       16318464,        1023.446,           2.199, [16, 8, 8, 16]],
+#     [       22609920,        1036.637,           2.362, [16, 8, 8, 24]],
+#     [       28901376,        1045.869,           2.262, [16, 8, 8, 32]],
+#     [       41484288,        1057.629,          14.544, [16, 8, 8, 48]],
+# ]
 
+# data_8thr_threads = [
+#     [       10027008,        1017.359,           1.066, [16, 8, 8, 8]],
+#     [       13172736,        1020.857,           1.016, [16, 8, 8, 12]],
+#     [       16318464,        1028.539,           0.939, [16, 8, 8, 16]],
+#     [       22609920,        1050.299,           2.221, [16, 8, 8, 24]],
+#     [       28901376,        1121.790,           3.720, [16, 8, 8, 32]],
+#     [       41484288,        1500.529,          10.945, [16, 8, 8, 48]],
+# ]
+
+
+# 16x16x16x16
 data_8thr_sockets = [
-    [       10027008,        1012.230,           1.774, [16, 8, 8, 8]],
-    [       13172736,        1015.779,           2.485, [16, 8, 8, 12]],
-    [       16318464,        1023.446,           2.199, [16, 8, 8, 16]],
-    [       22609920,        1036.637,           2.362, [16, 8, 8, 24]],
-    [       28901376,        1045.869,           2.262, [16, 8, 8, 32]],
-    [       41484288,        1057.629,          14.544, [16, 8, 8, 48]],
+    [       40108032,        3984.344,          57.330, [16, 16, 16, 8]],
+    [       52690944,        4051.156,          50.337, [16, 16, 16, 12]],
+    [       65273856,        4287.037,          31.062, [16, 16, 16, 16]],
+    [       90439680,        4450.687,          25.367, [16, 16, 16, 24]],
+    [      115605504,        5238.331,          36.409, [16, 16, 16, 32]],
+    [      165937152,        6934.767,          27.885, [16, 16, 16, 48]],
 ]
 
 data_8thr_threads = [
-    [       10027008,        1017.359,           1.066, [16, 8, 8, 8]],
-    [       13172736,        1020.857,           1.016, [16, 8, 8, 12]],
-    [       16318464,        1028.539,           0.939, [16, 8, 8, 16]],
-    [       22609920,        1050.299,           2.221, [16, 8, 8, 24]],
-    [       28901376,        1121.790,           3.720, [16, 8, 8, 32]],
-    [       41484288,        1500.529,          10.945, [16, 8, 8, 48]],
+    [       40108032,        4111.664,           7.821, [16, 16, 16, 8]],
+    [       52690944,        4179.769,          43.423, [16, 16, 16, 12]],
+    [       65273856,        4318.423,          32.492, [16, 16, 16, 16]],
+    [       90439680,        5219.628,          31.825, [16, 16, 16, 24]],
+    [      115605504,        6427.413,          73.932, [16, 16, 16, 32]],
+    [      165937152,        8159.567,          55.022, [16, 16, 16, 48]],
 ]
 
-# {
-#     1:[
-#         [3440640    ,    2649.940  ,         2.716 , [8, 8, 8, 4]],
-#         [5013504    ,    2705.936   ,        4.622 , [8, 8, 8, 8]],
-#         [6586368    ,    2798.551   ,        5.553  ,[8, 8, 8, 12]],
-#         [8159232     ,   2916.289      ,    12.388  ,[8, 8, 8, 16]],
-#         [11304960    ,    3139.590    ,       8.647 , [8, 8, 8, 24]],
-#         [14450688    ,    3279.491    ,       6.519,  [8, 8, 8, 32]],
-#         [20742144    ,    4198.875    ,      13.946,  [8, 8, 8, 48] ],
-#     ],
-#     2:[
-#         [3440640  ,      1331.650      ,     1.011 , [8, 8, 8, 4]],
-#         [5013504   ,     1361.989      ,     2.430 , [8, 8, 8, 8]],
-#         [6586368    ,    1423.532      ,     3.186 , [8, 8, 8, 12]],
-#        [ 8159232    ,    1489.055     ,      6.434 , [8, 8, 8, 16]],
-#        [11304960     ,   1625.998      ,     8.377  ,[8, 8, 8, 24]],
-#        [14450688     ,   1763.965      ,     5.314  ,[8, 8, 8, 32]],
-#        [20742144     ,   2373.417       ,    7.301 , [8, 8, 8, 48]],
-#     ],
-#     4:[
-#         [3440640       ,  670.814     ,      1.111 , [8, 8, 8, 4]],
-#         [5013504     ,    692.589     ,      2.525 , [8, 8, 8, 8]],
-#         [6586368   ,      733.391     ,      3.756 , [8, 8, 8, 12]],
-#         [8159232    ,     790.678     ,      3.423 , [8, 8, 8, 16]],
-#        [11304960    ,     891.027     ,      5.461 , [8, 8, 8, 24]],
-#        [14450688    ,    1096.042    ,       7.580  ,[8, 8, 8, 32]],
-#        [20742144    ,    1625.482     ,      5.995 , [8, 8, 8, 48]],
-#     ]
-# }
 
 x = [wilson_FLOP/dat[0] for dat in data_8thr_threads]
 
@@ -75,7 +66,7 @@ for dat in data_8thr_threads:
 
 plt.figure()
 
-plt.title("Roofline-style plot of a pseudo Dirac Wilson operator that allows\nvarying input data size, varying OMP_PLACES, on hpd-node-002")
+plt.title(f"Roofline plot of a pseudo Wilson Dirac operator that allows varying input\ndata size, {base_lattice_str} grid, varying OMP_PLACES, on hpd-node-002")
 
 
 plt.plot(x,y_threads, label=f"Wilson perf. for OMP_PLACES=threads")
@@ -85,7 +76,7 @@ plt.plot(x,[hpd_4core_FLOP_p_s for _ in x], label="peak perf. w/ 8 thr. on 4 cor
 plt.plot(x,[hpd_8core_FLOP_p_s for _ in x], label="peak perf. w/ 8 thr. on 8 cores")
 plt.plot(x,[hpd_bandwidth_B_p_s*xx for xx in x],label="bandwidth limited peak perf. on 4 cores")
 
-plt.axvline(x=wilson_FLOP/actual_wilson_data,label=f"Intensity of the correct operator ({base_lattice_str})",color="red",linestyle='--')
+plt.axvline(x=wilson_FLOP/actual_wilson_data,label=f"Intensity of the correct operator",color="red",linestyle='--')
 
 plt.legend()
 plt.xlabel("Intensity in FLOP/byte")
@@ -97,5 +88,5 @@ plt.yscale("log")
 plt.grid(which="both")
 # plt.yticks(range(0,110,10))
 
-plt.savefig("./test/testresults/wilson_roofline_hpd_8thr.pdf")
+plt.savefig(f"./test/testresults/wilson_roofline_hpd_8thr_{base_lattice_str}.pdf")
 
