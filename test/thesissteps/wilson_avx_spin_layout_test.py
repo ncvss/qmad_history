3dc2d1e4a5246b31208eb6365663e7fe7bf2121e
Nico <nico.voss@physik.uni-regensburg.de>
2025-08-22 18:42:03 +0200

    split data gen into batches

--- test/thesissteps/wilson_avx_spin_layout_test.py
+++ test/thesissteps/wilson_avx_spin_layout_test.py
@@ -1,6 +1,7 @@
 import torch
 import numpy as np
 import time
+import copy
 
 import gpt as g
 import qcd_ml
@@ -10,65 +11,96 @@ from qmad_history import compat, wilson, clover, settings, wilson_roofline
 
 num_threads = torch.get_num_threads()
 
-# split measurement into batches
+# split measurement into n_batch batches
 # we alternate between operators and lattice dimensions
-# and make n_batch measurements for each
 n_measurements = 200
+n_batch = 5
+assert n_measurements%n_batch == 0
+n_batchlen = n_measurements//n_batch
 n_warmup = 20
 
 mass = -0.5
 kappa = 1.0/2.0/(mass + 4.0)
 csw = 1.0
 
-print(n_measurements,"repetitions")
+print(n_measurements,"repetitions in",n_batch,"batches")
 print("mass =", mass)
 
 rng = g.random("thesis")
 
 start_grid = [4,4,2,4]
-n_vols = 12
+n_vols = 14
+all_grids = []
+for i in range(n_vols):
+    start_grid[(i+2)%4] *= 2
+    all_grids.append(copy.copy(start_grid))
+
 vols = [4*4*4*4*2**ii for ii in range(n_vols)]
-names = ["mtsg","tmgs"]
+names = ["mtsg","tmgs","mtsg_v","tmgs_v"]
 
 results = {vv:{na:np.zeros(n_measurements) for na in names} for vv in vols}
 
-for L_incr in range(n_vols):
-    start_grid[(L_incr+2)%4] *= 2
-    vol = start_grid[0]*start_grid[1]*start_grid[2]*start_grid[3]
+# split data generation into batches that each iterate over all sites
+# it does not work without that, pytorch does some strange things
+
+for nb in range(0,n_measurements,n_batchlen):
+    print("\ncurrent batch:",nb,flush=True)
+    print("current grid layout: ")
+    for L_incr in range(n_vols):
+        cgrid = all_grids[L_incr]
+        vol = cgrid[0]*cgrid[1]*cgrid[2]*cgrid[3]
+        print(cgrid,end=" ",flush=True)
+
+        # initialise the fields for this volume
+        U_gpt = g.qcd.gauge.random(g.grid(cgrid, g.double), rng)
+        grid = U_gpt[0].grid
+        v_gpt = rng.cnormal(g.vspincolor(grid))
+
+        U_mtsg = torch.tensor(compat.lattice_to_array(U_gpt))
+        v_mtsg = torch.tensor(compat.lattice_to_array(v_gpt))
+        v_tmgs = torch.permute(v_mtsg,(0,1,2,3,5,4)).contiguous()
+
+        dw_mtsg = wilson.wilson_hop_mtsg(U_mtsg,mass)
+        dw_tmgs = wilson.wilson_hop_tmgs(U_mtsg,mass)
+        dw_ref = qcd_ml.qcd.dirac.dirac_wilson(U_mtsg,mass)
+
+        for n in range(n_warmup):
+            res_mtsg = dw_mtsg.tmsgMh(v_mtsg)
+            res_tmgs = dw_tmgs.tmgsMh(v_tmgs)
+            res_mtsg_v = dw_mtsg.avx_tmsgMhs(v_mtsg)
+            res_tmgs_v = dw_tmgs.avx_tmgsMhs(v_tmgs)
+            if n == 0 and nb == 0:
+                res_ref = dw_ref(v_mtsg)
+                res_tmgs_back = torch.permute(res_tmgs,(0,1,2,3,5,4))
+                res_tmgs_v_back = torch.permute(res_tmgs_v,(0,1,2,3,5,4))
+                print("computations equal:",[torch.allclose(res_ref,res_c) for res_c in [res_mtsg,res_mtsg_v,res_tmgs_back,res_tmgs_v_back]])
+
+        for n in range(nb,nb+n_batchlen):
+            start = time.perf_counter_ns()
+            res_mtsg = dw_mtsg.tmsgMh(v_mtsg)
+            stop = time.perf_counter_ns()
+            results[vol]["mtsg"][n] = stop - start
+
+            start = time.perf_counter_ns()
+            res_tmgs = dw_tmgs.tmgsMh(v_tmgs)
+            stop = time.perf_counter_ns()
+            results[vol]["tmgs"][n] = stop - start
+
+            start = time.perf_counter_ns()
+            res_mtsg_v = dw_mtsg.avx_tmsgMhs(v_mtsg)
+            stop = time.perf_counter_ns()
+            results[vol]["mtsg_v"][n] = stop - start
+
+            start = time.perf_counter_ns()
+            res_tmgs_v = dw_tmgs.avx_tmgsMhs(v_tmgs)
+            stop = time.perf_counter_ns()
+            results[vol]["tmgs_v"][n] = stop - start
+
+
+for vol,cgrid in zip(vols,all_grids):
     print("---")
     print(vol,"sites")
-    print("grid layout",start_grid)
-
-    # initialise the fields for this volume
-    U_gpt = g.qcd.gauge.random(g.grid(start_grid, g.double), rng)
-    grid = U_gpt[0].grid
-    v_gpt = rng.cnormal(g.vspincolor(grid))
-
-    U_mtsg = torch.tensor(compat.lattice_to_array(U_gpt))
-    v_mtsg = torch.tensor(compat.lattice_to_array(v_gpt))
-    v_tmgs = torch.permute(v_mtsg,(0,1,2,3,5,4)).contiguous()
-
-    dw_mtsg = wilson.wilson_hop_mtsg(U_mtsg,mass)
-    dw_tmgs = wilson.wilson_hop_tmgs(U_mtsg,mass)
-
-    for n in range(n_warmup):
-        res_mtsg = dw_mtsg.avx_tmsgMhs(v_mtsg)
-        res_tmgs = dw_tmgs.avx_tmgsMhs(v_tmgs)
-        if n == 0:
-            res_tmgs_back = torch.permute(res_tmgs,(0,1,2,3,5,4))
-            print("computations equal:",torch.allclose(res_mtsg,res_tmgs_back))
-
-    for n in range(n_measurements):
-        start = time.perf_counter_ns()
-        res_mtsg = dw_mtsg.avx_tmsgMhs(v_mtsg)
-        stop = time.perf_counter_ns()
-        results[vol]["mtsg"][n] = stop - start
-
-        start = time.perf_counter_ns()
-        res_tmgs = dw_tmgs.avx_tmgsMhs(v_tmgs)
-        stop = time.perf_counter_ns()
-        results[vol]["tmgs"][n] = stop - start
-    
+    print("grid layout",cgrid)
     for na in names:
         result_sort = np.sort(results[vol][na])[:n_measurements//5]
         print(na,"time mean:",np.mean(result_sort)/1000,"µs")
