92d40202a7e667f439f6c528ac801e7ae57f7a07
Nico <nico.voss@physik.uni-regensburg.de>
2025-09-10 19:14:02 +0200

    Create nogauge_test.py

---
+++ test/nogauge_test.py
@@ -0,0 +1,24 @@
+import torch
+import qcd_ml
+from qmad_history import clover
+import time
+
+lat_dim = [16,16,16,16]
+mass = -0.5
+csw = 1.0
+
+print("test if wilson clover works on gpu")
+print("lattice:", lat_dim)
+print("mass:",mass)
+
+U = torch.randn([4]+lat_dim+[3,3], dtype=torch.cdouble)
+v = torch.randn(lat_dim+[4,3], dtype=torch.cdouble)
+
+dwc = qcd_ml.qcd.dirac.dirac_wilson_clover(U, mass, csw)
+dwc2 = clover.wilson_clover_hop_mtsg(U, mass, csw)
+
+dwcv = dwc(v)
+dwcv2 = dwc2.debug_cuda(v)
+
+print(torch.allclose(dwcv,dwcv2))
+# ergebnis: gleich, auch wenn es keine echten eichfelder sind
