dd34983fbeedfc0436f1b61068b4754007235554
Nico <nico.voss@physik.uni-regensburg.de>
2025-09-15 13:03:29 +0200

    Update qcd_ml_clover_cuda.py

--- test/qcd_ml_clover_cuda.py
+++ test/qcd_ml_clover_cuda.py
@@ -16,35 +16,35 @@ print("mass:",mass)
 print("csw:",csw)
 
 
-# lat_dim_1 = [16,16,16,16]
-# print("lattice 1:", lat_dim_1)
+lat_dim_1 = [16,16,16,16]
+print("lattice 1:", lat_dim_1)
+
+U = torch.empty([4]+lat_dim_1+[3,3], dtype=torch.cdouble)
+U[:,:,:,:,:] = torch.eye(3, dtype=torch.cdouble)
+
+v = torch.randn(lat_dim_1+[4,3], dtype=torch.cdouble)
+
 
 U2 = torch.tensor(np.load("./test/1500.config.npy"), dtype=torch.cdouble)
 lat_dim_2 = list(U2.shape[1:5])
 print("lattice 2:", lat_dim_2)
 
-# v = torch.randn(lat_dim_1+[4,3], dtype=torch.cdouble)
+# v2 = torch.zeros(lat_dim_2+[4,3], dtype=torch.cdouble)
+# v2[0,1,0,0,1,2] = 1
+v2 = torch.randn(lat_dim_2+[4,3], dtype=torch.cdouble)
 
-# U = torch.empty([4]+lat_dim_1+[3,3], dtype=torch.cdouble)
-# U[:,:,:,:,:] = torch.eye(3, dtype=torch.cdouble)
-
-v2 = torch.zeros(lat_dim_2+[4,3], dtype=torch.cdouble)
-v2[0,1,0,0,1,2] = 1
-# v2 = torch.randn(lat_dim_2+[4,3], dtype=torch.cdouble)
-
-# Ucu = U.to(cuda0)
-# vcu = v.to(cuda0)
+Ucu = U.to(cuda0)
+vcu = v.to(cuda0)
 U2cu = U2.to(cuda0)
 v2cu = v2.to(cuda0)
 
-# torch.cuda.synchronize()
-# dwc = qcd_ml.qcd.dirac.dirac_wilson_clover(Ucu, mass, csw)
-# torch.cuda.synchronize()
-# dwc_sf = clover.wilson_clover_hop_mtsg_sigpre(Ucu, mass, csw)
-# torch.cuda.synchronize()
-# dwc_f = clover.wilson_clover_hop_mtsg(Ucu, mass, csw)
-# torch.cuda.synchronize()
-
+torch.cuda.synchronize()
+dwc = qcd_ml.qcd.dirac.dirac_wilson_clover(Ucu, mass, csw)
+torch.cuda.synchronize()
+dwc_sf = clover.wilson_clover_hop_mtsg_sigpre(Ucu, mass, csw)
+torch.cuda.synchronize()
+dwc_f = clover.wilson_clover_hop_mtsg(Ucu, mass, csw)
+torch.cuda.synchronize()
 
 dwc2 = qcd_ml.qcd.dirac.dirac_wilson_clover(U2cu, mass, csw)
 torch.cuda.synchronize()
@@ -57,45 +57,45 @@ torch.cuda.synchronize()
 # print("field strength:")
 # print(F[0,1,0,0])
 
-# res_py = dwc(vcu)
-# torch.cuda.synchronize()
-# res_sf = dwc_sf.cu_tsg_tn(vcu)
-# torch.cuda.synchronize()
-# res_f = dwc_f.cu_tsg(vcu)
-# torch.cuda.synchronize()
+res_py = dwc(vcu)
+torch.cuda.synchronize()
+res_sf = dwc_sf.cu_tsg_tn(vcu)
+torch.cuda.synchronize()
+res_f = dwc_f.cu_tsg(vcu)
+torch.cuda.synchronize()
 
 res2_py = dwc2(v2cu)
 torch.cuda.synchronize()
-for _ in range(3):
+for _ in range(1):
     res2_f = dwc2_f.cu_tsg(v2cu)
     torch.cuda.synchronize()
     res2_sf = dwc2_sf.cu_tsg_tn(v2cu)
     torch.cuda.synchronize()
 
-# res_py_cpu = res_py.cpu()
-# res_sf_cpu = res_sf.cpu()
-# res_f_cpu = res_f.cpu()
+res_py_cpu = res_py.cpu()
+res_sf_cpu = res_sf.cpu()
+res_f_cpu = res_f.cpu()
 res2_py_cpu = res2_py.cpu()
 res2_sf_cpu = res2_sf.cpu()
 res2_f_cpu = res2_f.cpu()
 torch.cuda.synchronize()
 
-print("result order: sigmaF, Fmunu")
-print("config 1500 field:")
-print("single fermion variable (0,1,0,0):")
-print(res2_py_cpu[0,1,0,0])
-print(res2_sf_cpu[0,1,0,0])
-print(res2_f_cpu[0,1,0,0])
-torch.cuda.synchronize()
+# print("result order: sigmaF, Fmunu")
+# print("config 1500 field:")
+# print("single fermion variable (0,1,0,0):")
+# print(res2_py_cpu[0,1,0,0])
+# print(res2_sf_cpu[0,1,0,0])
+# print(res2_f_cpu[0,1,0,0])
+# torch.cuda.synchronize()
 
-print("single fermion variable (0,1,1,0):")
-print(res2_py_cpu[0,1,1,0])
-print(res2_sf_cpu[0,1,1,0])
-print(res2_f_cpu[0,1,1,0])
-torch.cuda.synchronize()
+# print("single fermion variable (0,1,1,0):")
+# print(res2_py_cpu[0,1,1,0])
+# print(res2_sf_cpu[0,1,1,0])
+# print(res2_f_cpu[0,1,1,0])
+# torch.cuda.synchronize()
 
 print("result differences:")
-# print("config 1500:", torch.sum(torch.abs(res_py_cpu-res_sf_cpu)), torch.sum(torch.abs(res_py_cpu-res_f_cpu)))
+print("unit gauge:", torch.sum(torch.abs(res_py_cpu-res_sf_cpu)), torch.sum(torch.abs(res_py_cpu-res_f_cpu)))
 print("config 1500:", torch.sum(torch.abs(res2_py_cpu-res2_sf_cpu)), torch.sum(torch.abs(res2_py_cpu-res2_f_cpu)))
 
 # result for only Fmunu:
