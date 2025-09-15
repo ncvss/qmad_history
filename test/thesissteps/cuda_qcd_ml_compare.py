70dd11cf0809fc18ba66fd0952d5538c29fc5c70
Nico <nico.voss@physik.uni-regensburg.de>
2025-09-15 20:05:15 +0200

    add test to compare cuda qcd_ml

---
+++ test/thesissteps/cuda_qcd_ml_compare.py
@@ -0,0 +1,105 @@
+# evtl: vergleich meines cuda-clovers mit dem von qcd_ml auf cuda
+
+
+import torch
+import numpy as np
+import time
+import copy
+
+from qmad_history import clover
+import qcd_ml
+
+
+# split measurement into n_batch batches
+# we alternate between operators and lattice dimensions
+n_measurements = 200
+n_batch = 5
+assert n_measurements%n_batch == 0
+n_batchlen = n_measurements//n_batch
+n_warmup = 20
+
+mass = -0.5
+kappa = 1.0/2.0/(mass + 4.0)
+csw = 1.0
+
+print(n_measurements,"repetitions in",n_batch,"batches")
+print("mass =", mass)
+print("csw =",csw)
+
+cuda0 = torch.device("cuda")
+print("using device",cuda0)
+
+# this is on the gpu, so we need way larger lattices
+# but also, these lattices are currently too large
+start_grid = [8,8,4,8]
+start_vol = start_grid[0]*start_grid[1]*start_grid[2]*start_grid[3]
+n_vols = 10
+all_grids = []
+for i in range(n_vols):
+    start_grid[(i+2)%4] *= 2
+    all_grids.append(copy.copy(start_grid))
+
+vols = [start_vol*2 *2**ii for ii in range(n_vols)]
+names = ["qcdmlcuda","qmadcuda",]
+
+results = {vv:{na:np.zeros(n_measurements) for na in names} for vv in vols}
+
+# split data generation into batches that each iterate over all sites
+# it does not work without that, pytorch does some strange things
+
+for nb in range(0,n_measurements,n_batchlen):
+    print("\ncurrent batch:",nb,flush=True)
+    print("current grid layout: ")
+    for L_incr in range(n_vols):
+        cgrid = all_grids[L_incr]
+        vol = cgrid[0]*cgrid[1]*cgrid[2]*cgrid[3]
+        print(cgrid,end=" ",flush=True)
+
+        # initialise the fields for this volume
+        # gpt is not installed, so we have to use pseudo fields
+        U_cpu = torch.randn([4]+cgrid+[3,3], dtype=torch.cdouble)
+        v_cpu = torch.randn(cgrid+[4,3], dtype=torch.cdouble)
+
+        Ucu = U_cpu.to(cuda0)
+        vcu = v_cpu.to(cuda0)
+
+        dwc_py = qcd_ml.qcd.dirac.dirac_wilson_clover(Ucu, mass, csw)
+        torch.cuda.synchronize()
+        dwc_sf = clover.wilson_clover_hop_mtsg_sigpre(Ucu, mass, csw)
+
+
+        for n in range(n_warmup):
+            torch.cuda.synchronize()
+            res_py = dwc_py(vcu)
+            torch.cuda.synchronize()
+            res_sf = dwc_sf.cu_tsg_tn(vcu)
+            torch.cuda.synchronize()
+            # because these are pseudo fields, the results are not equal
+            # if n == 0 and nb == 0:
+            #     print("computations equal:",torch.allclose(res_f,res_sf))
+
+        for n in range(nb,nb+n_batchlen):
+            torch.cuda.synchronize()
+            start = time.perf_counter_ns()
+            res_py = dwc_py(vcu)
+            torch.cuda.synchronize()
+            stop = time.perf_counter_ns()
+            results[vol]["qcdmlcuda"][n] = stop - start
+
+            torch.cuda.synchronize()
+            start = time.perf_counter_ns()
+            res_sf = dwc_sf.cu_tsg_tn(vcu)
+            torch.cuda.synchronize()
+            stop = time.perf_counter_ns()
+            results[vol]["qmadcuda"][n] = stop - start
+
+
+for vol,cgrid in zip(vols,all_grids):
+    print("---")
+    print(vol,"sites")
+    print("grid layout",cgrid)
+    for na in names:
+        result_sort = np.sort(results[vol][na])[:(n_measurements//5)]
+        print(na,"time mean:",np.mean(result_sort)/1000,"µs")
+        print(na+"_stdev:",np.std(result_sort)/1000,"µs")
+    
