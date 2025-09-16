d831f99d95b71b5489a8f2f24abf2599a98c478b
Nico <nico.voss@physik.uni-regensburg.de>
2025-09-16 19:37:25 +0200

    change number of threads

--- qmad_history/csrc/cuda/clover_hop.cu
+++ qmad_history/csrc/cuda/clover_hop.cu
@@ -95,7 +95,7 @@ at::Tensor dwc_hop_mtsg_cu_tsg_fpre (const at::Tensor& U_ten, const at::Tensor& 
     c10::complex<double>* result = result_ten.mutable_data_ptr<c10::complex<double>>();
 
     // allocate one thread for each vector component, in 1024-thread blocks
-    int threadnum = 512;
+    int threadnum = 512+256;
     int blocknum = (vvol+threadnum-1)/threadnum;
 
     // printf("threadnum vs lattice: %d %d\n", vol, threadnum*blocknum);
@@ -260,7 +260,7 @@ at::Tensor dwc_hop_mtsg_cu_tsg_sigpre (const at::Tensor& U_ten, const at::Tensor
     TORCH_INTERNAL_ASSERT(hops_ten.device().type() == at::DeviceType::CUDA);
 
     int vol = hops_ten.size(0);
-    int vvol = vol*12;
+    int vvol = vol*4*3;
 
     at::Tensor result_ten = torch::empty(v_ten.sizes(), v_ten.options());
     const c10::complex<double>* U = U_ten.const_data_ptr<c10::complex<double>>();
@@ -270,7 +270,7 @@ at::Tensor dwc_hop_mtsg_cu_tsg_sigpre (const at::Tensor& U_ten, const at::Tensor
     c10::complex<double>* result = result_ten.mutable_data_ptr<c10::complex<double>>();
 
     // allocate one thread for each vector component, in 1024-thread blocks
-    int threadnum = 512;
+    int threadnum = 512+256;
     int blocknum = (vvol+threadnum-1)/threadnum;
     // for clover, only 2 threads per site, one for each 6x6 block
     int cl_blocknum = (vol*2+threadnum-1)/threadnum;
